a4b5393102b83581c92d489271c9b863c3bf0e45
Debanjana <debanjana.9806@gmail.com>
2022-11-20 15:25:53 -0800

    submit-done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+
+
+import React, {useState} from "react";
 
 function App() {
+  const [para,setPara] = useState(true)
+  const handleClick=()=>{
+    setPara(false)
+  }
   return (
     <div id="main">
       // Do not alter the main div
+    {para ? (<button id="click" onClick={handleClick}>
+      click me
+    </button>):(<p id="para">
+    Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+    </p>)}
     </div>
   );
 }
